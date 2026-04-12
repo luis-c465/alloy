1cd4d81c5d81fb43699236da79c4337dd8be4537
Luis Canada <dev@luisc.me>
2026-04-12 15:02:15 -0400

    Add back react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { queryClient } from "./lib/query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* code goes here */}
+			<QueryClientProvider client={queryClient}>
+        {/* Your app components go here */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
   </React.StrictMode>,
 );
